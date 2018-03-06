549855a39b5bca4e89ab8957fc9dd5b98d424536
Anders Sjöbom <anders.sjobom@live.se>
2018-03-06 02:36:16 +0100

    feat: integrated spam limit feature to random gif command

--- commands/random.js
+++ commands/random.js
@@ -1,55 +1,41 @@
 const util = require("../util.js");
 const request = require("request");
+const spam = require("../internal/spam.js");
+const config = require("../config/config.json");
 
 var baseURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=";
 
 exports.run = (client, message, args) => {
-    //addArgumentsToUrl(args, baseURL, function(url) {
-        var url = baseURL;
+        spam.check_authors_spam_time_limit(message)
+        .then(function (data) {
+            // If the author has not been spamming he/she can now request a gif!
+            var url = baseURL;
+            
+            if(args != undefined){
+                for(arg in args){
+                    url += args[arg];
+                    if(arg < args.length - 1){
+                        url += "+";
+                    }
+                }
+            }
+
+            try{
+                util.jsonRequest(url, function(data){
+                    gif_url = data["data"]["image_original_url"];
+                    message.channel.send(util.embedPicture(gif_url));
+                });    
+            } catch(error){
+                console.error(error);
+            }
+        })
+        .catch(function rejected(seconds_left) {
+            // Author have been spamming and is not allowed to request gifs!
+            //message.
+            reply = message.member + " " + seconds_left + " seconds until you can request a gif again ";
+            reply += " (" + config.spam_message_limit + " gifs within " + config.spam_time_limit + " seconds allowed)";
+            message.delete();
+            message.channel.send(reply);
+        });
         
-        if(args != undefined){
-            for(arg in args){
-                url += args[arg];
-                if(arg < args.length - 1){
-                    url += "+";
-                }
-            }
-        }
-
-        try{
-            util.jsonRequest(url, function(data){
-                gif_url = data["data"]["image_original_url"];
-                /*
-                if(gif_url === null){
-                    util.jsonRequest(baseURL, function(data){
-                        gif_url = data["data"]["image_original_url"];
-                    });
-                }
-                */
-                message.channel.send(util.embedPicture(gif_url));
-            });    
-        } catch(error){
-            console.error(error);
-        }
-    //});
-    
 }
-
-/*
-function addArgumentsToUrl(fArgs, fUrl, callback){
-    console.log(fArgs);
-    if(fArgs === []){
-        console.log("I WAS HERE");
-        fUrl += "rebecca+black";
-    }
-    else{
-        for(arg in fArgs){
-            fUrl += fArgs[arg];
-            if(arg < fArgs.length - 1){
-                fUrl += "+";
-            }
-        }
-    }
-    callback(fUrl);
-}
-*/
