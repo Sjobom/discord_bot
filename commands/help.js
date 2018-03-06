f1f4a04e0048248e1b23941be518bb663edf2e89
Anders Sjöbom <anders.sjobom@live.se>
2018-03-06 13:39:02 +0100

    fix: bug fixes to help command
    * The help command wasn't working after the picture links and sound features were removed earlier

--- commands/help.js
+++ commands/help.js
@@ -9,27 +9,8 @@ exports.run = (client, message, args) => {
             let commandName = file.split(".")[0];
             commands += config.prefix + commandName + "\n";
         });
-        const picture_links = require("../pictures/picture_links.json");
-        let pictures = "";
-        for(pictureName in picture_links){
-            pictures += config.prefix + pictureName + "\n";
-        };
+        message.channel.send("Here are the things that I can do:\n" + commands).catch(console.error);
 
-        var sounds = "";
-        let summary = "";
-        fs.readdir(config["soundFolder"], (err, files) => {
-            files.forEach(file => {
-                sound = file.split(".")[0];
-                sounds += config.prefix + sound + "\n";
-            });
-            
-            summary += "COMMANDS:\n" + commands;
-            //summary += "PICTURES:\n" + pictures;
-            summary += "SOUNDS:\n" + sounds;
-            message.channel.send(summary).catch(console.error);
-        });
-
-        
     });
 
     
